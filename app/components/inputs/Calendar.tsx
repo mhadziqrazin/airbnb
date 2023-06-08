b0611d8b08d836e4533371c1a3ea7e1054adba07
mhadziqrazin <mhadziq0403@gmail.com>
2023-06-09 00:28:36 +0700

    fix: type error

--- app/components/inputs/Calendar.tsx
+++ app/components/inputs/Calendar.tsx
@@ -1,11 +1,11 @@
-import { DateRange, Range } from "react-date-range"
+import { DateRange, Range, RangeKeyDict } from "react-date-range"
 import "react-date-range/dist/styles.css"
 import "react-date-range/dist/theme/default.css"
 
 interface CalendarProps {
   value: Range
   disabledDates: Date[]
-  onChange: (value) => void
+  onChange: (value: RangeKeyDict) => void
 }
 
 const Calendar: React.FC<CalendarProps> = ({
