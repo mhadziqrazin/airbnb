2b7b42b844cfea934b95dc0a91bdd49875628bd6
mhadziqrazin <mhadziq0403@gmail.com>
2023-06-11 22:33:46 +0700

    style: search label based on fitlers

--- app/components/navbar/Search.tsx
+++ app/components/navbar/Search.tsx
@@ -1,10 +1,49 @@
 'use client'
 
+import useCountries from '@/app/hooks/useCountries'
 import useSearchModal from '@/app/hooks/useSearchModal'
+import { differenceInDays } from 'date-fns'
+import { useSearchParams } from 'next/navigation'
+import { useMemo } from 'react'
 import { BiSearch } from 'react-icons/bi'
 
 const Search = () => {
   const searchModal = useSearchModal()
+  const params = useSearchParams()
+  const { getByValue } = useCountries()
+
+  const locationValue = params?.get('locationValue')
+  const startDate = params?.get('startDate')
+  const endDate = params?.get('endDate')
+  const guestCount = params?.get('guestCount')
+
+  const locationLabel = useMemo(() => {
+    if (locationValue) {
+      return getByValue(locationValue as string)?.label
+    }
+    return 'Anywhere'
+  }, [locationValue, getByValue])
+
+  const durationLabel = useMemo(() => {
+    if (!startDate || !endDate) {
+      return 'Any Week'
+    }
+
+    const start = new Date(startDate as string)
+    const end = new Date(endDate as string)
+    let diff = differenceInDays(end, start)
+    
+    diff = diff === 0 ? 1 : diff
+    return `${diff} Days`
+
+  }, [startDate, endDate])
+
+  const guestLabel = useMemo(() => {
+    if (guestCount) {
+      return `${guestCount} Guests`
+    }
+    return 'Add Guests'
+  }, [guestCount])
 
   return (
     <div
@@ -13,14 +52,14 @@ const Search = () => {
     >
       <div className="flex flex-row items-center justify-between">
         <div className="text-sm font-semibold px-6">
-          Anywhere
+          {locationLabel}
         </div>
         <div className="hidden sm:block text-sm font-semibold px-6 border-x-[1px] dark:border-neutral-700 flex-1 text-center">
-          Any Week
+          {durationLabel}
         </div>
         <div className="text-sm pl-6 pr-2 text-gray-600 dark:text-gray-300 flex flex-row items-center gap-3">
           <div className="hidden sm:block">
-            Add Guests
+            {guestLabel}
           </div>
           <div className="bg-rose-500 rounded-full p-2 text-white">
             <BiSearch size={18} />
